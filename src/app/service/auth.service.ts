a7cb46c02cc236919d10657ed374fc6dac79f1f7
Rathik Pratap <52971518+rathikpratap@users.noreply.github.com>
2024-02-07 18:01:38 +0530

    environment commit

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, map, throwError } from 'rxjs';
+import { appConfig } from 'src/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,48 +13,48 @@ export class AuthService {
   constructor(private http:HttpClient) { }
 
   signup(data:any):Observable<any>{
-    return this.http.post('http://localhost:5000/auth/register', data);
+    return this.http.post(`${appConfig.apiUrl}/auth/register`, data);
   }
 
   signin(loginData:any):Observable<any>{
-    return this.http.post('http://localhost:5000/auth/login', loginData);
+    return this.http.post(`${appConfig.apiUrl}/auth/login`, loginData);
   }
 
   addcustomer(customerData:any):Observable<any>{
-    return this.http.post('http://localhost:5000/auth/customer', customerData)
+    return this.http.post(`${appConfig.apiUrl}/auth/customer`, customerData)
   }
 
   getCustData(){
-    return this.http.get('http://localhost:5000/auth/list');
+    return this.http.get(`${appConfig.apiUrl}/auth/list`);
   }
 
   getCompleteProjects(){
-    return this.http.get('http://localhost:5000/auth/completeProject');
+    return this.http.get(`${appConfig.apiUrl}/auth/completeProject`);
   }
 
   getAllProjects(){
-    return this.http.get('http://localhost:5000/auth/allOngoingProjects');
+    return this.http.get(`${appConfig.apiUrl}/auth/allOngoingProjects`);
   }
 
   getAllCompleteProjects(){
-    return this.http.get('http://localhost:5000/auth/allCompleteProjects');
+    return this.http.get(`${appConfig.apiUrl}/auth/allCompleteProjects`);
   }
 
     getProfile(){
     let headers = {
       'Authorization': "Bearer " + localStorage.getItem('token')
     }
-    return this.http.get('http://localhost:5000/auth/profile', {headers:headers})
+    return this.http.get(`${appConfig.apiUrl}/auth/profile`, {headers:headers})
   }
 
   updateCustomer(id:any, data:any):Observable<any>{
-    return this.http.put(`http://localhost:5000/auth/update/${id}`, data, {headers:this.httpHeaders}).pipe(
+    return this.http.put(`${appConfig.apiUrl}/auth/update/${id}`, data, {headers:this.httpHeaders}).pipe(
       catchError(this.handleError)
     )
   }
 
   getCustomer(id:any) :Observable<any>{
-    return this.http.get(`http://localhost:5000/auth/read-cust/${id}`,{headers:this.httpHeaders}).pipe(map((res:any)=>{
+    return this.http.get(`${appConfig.apiUrl}/auth/read-cust/${id}`,{headers:this.httpHeaders}).pipe(map((res:any)=>{
       return res || {}
     }), catchError(this.handleError)
   )}
