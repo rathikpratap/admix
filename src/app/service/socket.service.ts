7d3627bb63f8fecd58e89bf46ebcee0b0fe9359a
Rathik Pratap <52971518+rathikpratap@users.noreply.github.com>
2026-03-19 16:48:36 +0530

    socket Commit

--- src/app/service/socket.service.ts
+++ src/app/service/socket.service.ts
@@ -9,7 +9,8 @@ export class SocketService {
   private socket: Socket;
 
   constructor() {
-    this.socket = io('http://localhost:5000');
+    // this.socket = io('http://localhost:5000');
+    this.socket = io('https://www.login.admixmedia.in')
   }
   onReminder(): Observable<any> {
     return new Observable(observer => {
